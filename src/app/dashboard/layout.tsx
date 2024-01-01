ce188a8171135974ca0ede9f9976038143734558
prince ajuzie <princeajuzie1@gmail.com>
2024-01-01 16:20:30 +0100

    fix: error on nested layout i.e naming conversiton

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,16 +1,14 @@
-
 import React from "react";
-
 import NavbarWithMegaMenu from "@/components/navbar/Navbar";
-export default  function layout({
+
+export default function Layout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <>
       <NavbarWithMegaMenu />
-
       {children}
     </>
   );
